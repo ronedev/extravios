654a6711be6ec34156b2a2d9648005cc68ce83f0
Agustin notebook <ronedev25@gmail.com>
2022-06-19 02:25:34 -0300

    add: login and logout with auth0 plataform

--- frontend/components/HeaderHome.tsx
+++ frontend/components/HeaderHome.tsx
@@ -1,12 +1,40 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { useUser } from "@auth0/nextjs-auth0";
+import {
+  Flex,
+  Text,
+  Heading,
+  Link,
+  Spacer,
+  Image,
+  AvatarGroup,
+  Avatar,
+  MenuButton,
+  Menu,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
 import React from "react";
 
 type Props = {};
 
 const HeaderHome = (props: Props) => {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Cargando usuario...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  const userName = user?.name?.split(" ")[0];
+
   return (
-    <Flex alignItems="center" marginTop={4} marginBottom={2} color="white" as={'header'}>
+    <Flex
+      alignItems="center"
+      marginTop={4}
+      marginBottom={2}
+      color="white"
+      as={"header"}
+    >
       <Heading
+        display={"flex"}
         fontWeight={600}
         fontSize={{ base: "xl", sm: "2xl", md: "4xl" }}
         lineHeight={"180%"}
@@ -14,6 +42,31 @@ const HeaderHome = (props: Props) => {
       >
         Extravios
       </Heading>
+      {user ? (
+        <>
+          <Spacer />
+          <Menu>
+            <MenuButton as={"button"}>
+              <AvatarGroup spacing={"1rem"}>
+                <Avatar src={user.picture ? user.picture : undefined} bg={"transparent"} alt={userName} as={'img'} />
+                <Text fontSize={"xl"}>{userName}</Text>
+              </AvatarGroup>
+            </MenuButton>
+            <MenuList>
+              <Link href="/api/auth/logout">
+                <MenuItem>Cerrar sesión</MenuItem>
+              </Link>
+            </MenuList>
+          </Menu>
+        </>
+      ) : (
+        <>
+          <Spacer />
+          <Link href="/api/auth/login">
+            <Text>Ingresar</Text>
+          </Link>
+        </>
+      )}
     </Flex>
   );
 };
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,16 +3,19 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./../theme";
 import NextNProgress from "nextjs-progressbar";
 import Layout from "../components/Layout";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ChakraProvider theme={theme}>
-        <NextNProgress color="#29D" />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
+      <UserProvider>
+        <ChakraProvider theme={theme}>
+          <NextNProgress color="#29D" />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </UserProvider>
     </>
   );
 }
