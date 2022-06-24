d39b9eb542eb5db0fcf25b8f197d6f5da4a49f99
Agustin notebook <ronedev25@gmail.com>
2022-06-24 16:00:58 -0300

    feat: validation with yup in new post page

--- frontend/pages/post/new.tsx
+++ frontend/pages/post/new.tsx
@@ -1,12 +1,34 @@
-import { Button, Flex, Input, Stack } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
+import { Box, Button, Center, Flex, Input, Stack, useToast } from "@chakra-ui/react";
 import { useFormik } from "formik";
 import axios from "axios";
 import { server } from "../../config";
+import * as Yup from "yup";
 
-type Props = {};
+const New = () => {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-const New = (props: Props) => {
+  const toast = useToast();
+
+  const newPostSchema = Yup.object().shape({
+    title: Yup.string()
+      .min(10, "Al menos 10 caracteres")
+      .max(50, "Máximo 50 caracteres")
+      .required("Requerido"),
+    description: Yup.string()
+      .min(20, "Al menos 20 caracteres")
+      .max(250, "Máximo 250 caracteres")
+      .required("Requerido"),
+    city: Yup.string()
+      .min(6, "Al menos 6 caracteres")
+      .max(100, "Máximo 100 caracteres")
+      .required("Requerido"),
+    phone: Yup.string()
+      .matches(/^\d{7,14}$/, 'Número de télefono no válido' )
+      .required("Requerido"),
+  });
+
   const formik = useFormik({
     initialValues: {
       title: "",
@@ -14,15 +36,31 @@ const New = (props: Props) => {
       city: "",
       phone: "",
     },
-    onSubmit: (values, { resetForm }) => {
-      axios.post(`${server}/api/posts`, values)
-        .then((res)=>{
-            console.log(res)
-            resetForm()
+    validationSchema: newPostSchema,
+    onSubmit: async (values, { resetForm }) => {
+      setError("");
+      setLoading(true);
+
+      await axios
+        .post(`${server}/api/posts`, values)
+        .then((res) => {
+          resetForm();
+          toast({
+            position: "top",
+            title: "¡Extravío creado!",
+            description: "Tu publicación fue creada con éxito",
+            status: "success",
+            duration: 4000,
+            isClosable: true,
+          });
         })
-        .catch((err)=>{
-            console.log(err)
+        .catch((err) => {
+          console.log(err);
+          setError(err?.message);
         })
+        .finally(() => {
+          setLoading(false);
+        });
 
       resetForm();
     },
@@ -43,43 +81,67 @@ const New = (props: Props) => {
       <h2>Nuevo extravío</h2>
 
       <form onSubmit={formik.handleSubmit}>
-        <Stack w="md" mt={4} gap={4}>
-          <Input
-            placeholder="Título"
-            size="md"
-            id="title"
-            name="title"
-            onChange={formik.handleChange}
-            value={formik.values.title}
-          />
-          <Input
-            placeholder="Ciudad"
-            size="md"
-            id="city"
-            name="city"
-            onChange={formik.handleChange}
-            value={formik.values.city}
-          />
-          <Input
-            placeholder="Número de telefono"
-            size="md"
-            id="phone"
-            name="phone"
-            onChange={formik.handleChange}
-            value={formik.values.phone}
-          />
-          <Input
-            placeholder="Descripción"
-            size="md"
-            id="description"
-            name="description"
-            onChange={formik.handleChange}
-            value={formik.values.description}
-          />
-
+        <Stack w="md" mt={4} gap={1}>
+          <Box gap={1}>
+            <Input
+              placeholder="Título"
+              size="md"
+              id="title"
+              name="title"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.title}
+            />
+            {formik.touched.title && formik.errors.title ? (
+              <Center color="red.600">{formik.errors.title}</Center>
+            ): null}
+          </Box>
+          <Box gap={1}>
+            <Input
+              placeholder="Ciudad"
+              size="md"
+              id="city"
+              name="city"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.city}
+            />
+            {formik.touched.city && formik.errors.city && (
+              <Center color="red.600">{formik.errors.city}</Center>
+            )}
+          </Box>
+          <Box gap={1}>
+            <Input
+              placeholder="Número de telefono"
+              size="md"
+              id="phone"
+              name="phone"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.phone}
+            />
+            {formik.touched.phone && formik.errors.phone && (
+              <Center color="red.600">{formik.errors.phone}</Center>
+            )}
+          </Box>
+          <Box gap={1}>
+            <Input
+              placeholder="Descripción"
+              size="md"
+              id="description"
+              name="description"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.description}
+            />
+            {formik.touched.description && formik.errors.description && (
+              <Center color="red.600">{formik.errors.description}</Center>
+            )}
+          </Box>
           <Button colorScheme="blue" type="submit">
             Publicar
           </Button>
+          {error && <Center color="red.600">{error}</Center>}
         </Stack>
       </form>
     </Flex>
